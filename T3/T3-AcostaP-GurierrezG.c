6906fe3b997d804d06d7a54d066a5853c052d4bd
Alan Acosta <alanomar460@gmail.com>
2023-03-16 21:32:33 -0600

    T3 Vfinal????

--- T3/T3-AcostaP-GurierrezG.c
+++ T3/T3-AcostaP-GurierrezG.c
@@ -5,16 +5,18 @@
 FILE *archivoB;
 
 // Estructura Producto
-struct Producto
+typedef struct 
 {
     int codigoPrt;
     char descripcionPrt[20];
     float precioPrt;
     int existenciaPrt;
-}P1, P2, P3, P4, arrayP[8]; // Definición de cuatro variables y un arreglo de 8 variables de tipo Producto
+} Producto;
+
+Producto P1, P2, P3, P4, arrayP[8], datosArchivo[7]; // Definición de cuatro variables y un arreglo de 8 variables de tipo Producto
 
 // Funciones
-void rellenarPrt(int codigo, char descripcion[], float precio, int existencia, struct Producto *P) // Asgina los datos a una variable de tipo Producto 
+void rellenarPrt(int codigo, char descripcion[], float precio, int existencia, Producto *P) // Asgina los datos a una variable de tipo Producto 
 {
     P->codigoPrt = codigo; 
     strcpy(P->descripcionPrt, descripcion);
@@ -22,19 +24,14 @@ void rellenarPrt(int codigo, char descripcion[], float precio, int existencia, s
     P->existenciaPrt = existencia;
 }
 
-void mostrarPrt(struct Producto P) // Muestra los datos de una variable de tipo Producto en pantalla
+void mostrarPrt(Producto P) // Muestra los datos de una variable de tipo Producto en pantalla
 {
     printf("  %d\t\t%s\t$%.2f\t\t    %d\n", P.codigoPrt, P.descripcionPrt, P.precioPrt, P.existenciaPrt);
 }
 
-void guardarArchivoB(struct Producto P) 
-{
-    fprintf(archivoB, "  %d \t\t%s  \t\t$%.2f    \t\t    %d\n", P.codigoPrt, P.descripcionPrt, P.precioPrt, P.existenciaPrt); // Cambiar a fwrite
-}
-
 int main()
 {
-    struct Producto P5; // Definición de una quinta variable de tipo Producto
+    Producto P5; // Definición de una quinta variable de tipo Producto
     
     // Asignación manual de datos a la quinta variable
     P5.codigoPrt = 10;
@@ -73,16 +70,21 @@ int main()
     for(int i = 0; i < 7; i ++)
         mostrarPrt(arrayP[i]);
     
-    // Abriendo archivo binario en modo escritura
-    archivoB = fopen("T3-AcostaP-GurierrezG.bin", "w");
-    
-    // Guardando el arreglo en el archivo binario
-    fprintf(archivoB, "Codigo\tDescripcion\t\tPrecio\t\tExistencia\n"); // Cambiar a fwrite
-    for(int i = 0; i < 7; i ++)
-        guardarArchivoB(arrayP[i]);
-        
-    // Cerrando archivo binario
+    // Guardando los datos en archivo binario
+    archivoB = fopen("T3-AcostaP-GurierrezG.bin", "wb");
+    fwrite(arrayP, sizeof(Producto), 7, archivoB);
     fclose(archivoB);
+
+    // Leyendo datos del archivo y guardandolos en array datosArchivo[]
+    archivoB = fopen("T3-AcostaP-GurierrezG.bin", "rb");
+    fread(datosArchivo, sizeof(Producto), 7, archivoB);
+    fclose(archivoB);
+
+    // Mostrando contenido del archivo
+    printf("\n.::Archivo::.\n");
+    printf("Codigo\t\tDescripcion\tPrecio\t\tExistencia\n");
+    for(int i = 0; i < 7; i ++)
+        mostrarPrt(datosArchivo[i]);
 
     return 0;
 }
