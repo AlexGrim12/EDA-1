e5a13324b3dbf2f478342bb4f6203eb67fad2f1b
Alan Acosta <alanomar460@gmail.com>
2023-03-24 23:47:23 -0600

    T5 V3 'con comentarios, falta revision'

--- T5/T5-AcostaA-GutierrezG.c
+++ T5/T5-AcostaA-GutierrezG.c
@@ -1,90 +1,111 @@
 #include <stdio.h>
-#define STACKSIZE 5
-typedef int item;
+#define MAXSTACK 5 // Máximo tamaño del stack
 
-item stack[STACKSIZE];
-int TOS = -1;
+int stack[MAXSTACK], stackCopia[MAXSTACK], TOS = -1, TOSC = -1;
 
-void push(item k)
+void push(int dato)
 {
     TOS ++;
 
-    if (TOS >= STACKSIZE)
+    if (TOS >= MAXSTACK) // Comprobar si el stack esta lleno
     {
-        printf("*** Estacionamiento lleno, ya no hay espacio *** \n\n");
+        printf("\t*** Estacionamiento lleno, ya no hay espacio *** \n\n");
         TOS --;
+        return;
     }
-    else 
-    {
-        stack[TOS] = k;
-        printf("Coche con placa %d insertado en el estacionamiento\n\n", k);
-    }
+    
+    stack[TOS] = dato; // Agregar dato al stack
+    printf("\t*** El coche con placa %d ingreso en el estacionamiento ***\n\n", dato);
 }
 
-item pop()
+void pop()
 {
-    item placa;
-
-    if (TOS == -1)
+    if (TOS == -1) // Comprobar si el stack esta vacio
     {
-        printf(" *** Estacionamiento vacio *** \n");
-        return -999;
+        printf("\n\t*** Estacionamiento vacio ***\n\n");
+        return;
     }
 
-    placa = stack[TOS];
-    TOS --;
-    return placa;
+    printf("\n\t*** El coche con placa %d salio del estacionamiento ***\n\n", stack[TOS]);
+    TOS --; // Eliminar dato del stack
+}
+
+void popDato(int dato)
+{
+    int flag = 0;
+
+    while (TOS != -1 && flag == 0)
+    {
+        if (stack[TOS] == dato) // Comprobar que el dato exista en el stack
+        {
+            printf("\t*** El coche con placa %d salio del estacionamiento ***\n\n", dato);
+            flag = 1;
+        }
+        else // Si en esa posicion no esta el dato, se transpasa a otro stack y se avanza la posicion
+        {
+            TOSC ++;
+            stackCopia[TOSC] = stack[TOS];
+            TOS --;
+        }
+    }
+
+    while (TOSC != -1) // Devolver datos del stack copia al stack original
+    {
+        stack[TOS] = stackCopia[TOSC];
+        TOS ++;
+        TOSC --;
+    }
+    
+    if (flag == 0) 
+        printf("\t*** No se encontro el coche con placa %d en el estacionamiento ***\n\n", dato);
+    else
+        TOS --;
 }
 
 void showStack()
 {
-    int k;
-
-    printf("\n*** Top of Stack = %d ***\n", TOS);
-    if (TOS == -1)
-        printf("*** Estacionamiento vacio ***");
-    else
+    if (TOS == -1) // Comprobar si hay elementos
+        printf("\n\t*** Estacionamiento vacio ***");
+    else // Imprimir datos
     {
-        for (k = TOS; k >= 0; k --)
-            printf("%d\t", stack[k]);
+        printf("\n\t*** Lugares ocupados = %d ***\n", TOS + 1);
+        for (int i = TOS; i > -1; i --)
+            printf("%d\t", stack[i]);
     }
     printf("\n\n");
 }
 
 int main()
 {
-    item placa;
-    int opcion;
-    printf("*** Bienvenido al Estacionamiento 'Champs' ***\n");
-    while (opcion != 4)
+    int placa, opcion;
+
+    printf("\t*** Bienvenido al Estacionamiento 'Champs' ***\n");
+    while (opcion != 5)
     {
-        printf("\t1. Push\n");
-        printf("\t2. Pop\n");
-        printf("\t3. Mostrar Stack\n");
-        printf("\t4. Salir\n");
-        printf("\tOpcion? ");
+        printf("1. Push\n2. Pop\n3. Pop de placa especifica\n4. Mostrar Stack\n5. Salir\nOpcion? "); // Menu 
         scanf("%d", &opcion);
         switch (opcion)
         {
-            case 1:
-                printf("\nPlaca del auto? ");
+            case 1: 
+                printf("\n\tPlaca del auto? ");
                 scanf("%d", &placa);
                 push(placa);
                 break;
-            case 2:
-                placa = pop();
-                if (placa != -999)
-                    printf("\nCoche con placa %d salio del estacionamiento\n\n");
+            case 2: pop(); break;
+            case 3: 
+                printf("\n\tPlaca del auto? ");
+                scanf("%d", &placa);
+                popDato(placa);
                 break;
-            case 3:
-                showStack();
-            default:
-                break;
+            case 4: showStack();
+            default: break;
         }
         
-        if (opcion > 4)
-            printf("\nOpcion no valida\n\n");   
+        if (opcion > 5) // Si la opcion es mayor a 5 se repite el menu
+            printf("\n\t*** Opcion no valida ***\n\n");   
     }
 
-    printf("\n*********** Fin del Programa ********\n");
+    printf("\n*** Fin del Programa ***\n"); // Cuando la opcion es 5 se acaba la ejecución
+
+    return 0;
 }
