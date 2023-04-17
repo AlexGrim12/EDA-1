2321e40ee1652d3e3560166cff8a36de7ab30a55
Alan Acosta <alanomar460@gmail.com>
2023-04-17 16:59:52 -0600

    T8 version preeliminar

--- T8-AcostaP-GutierrezG.c
+++ T8-AcostaP-GutierrezG.c
@@ -9,95 +9,78 @@ typedef struct Nodo {
 
 Nodo *lista = NULL;
 
-void insertarLista(Nodo **lista, int n) // Cambiar la firma de la función
-{
+void insertarLista(Nodo **lista, int n) { // Cambiar la firma de la función
     Nodo *nuevoNodo = (Nodo*) malloc(sizeof(Nodo));
     nuevoNodo->dato = n;
 
-    Nodo *aux1 = *lista, *aux2 = NULL; // Inicializar aux2 a NULL
+    Nodo *aux1 = *lista, *aux2 = NULL; // Inicializar aux2 como NULL
 
-    while (aux1 != NULL && aux1->dato < n)
-    {
+    while (aux1 != NULL && aux1->dato < n) {
         aux2 = aux1;
         aux1 = aux1->siguiente;
     }
     
-    if (aux2 == NULL) // Si el nodo a insertar es el primero
-        *lista = nuevoNodo;
-    else
-        aux2->siguiente = nuevoNodo;
+    if(aux2 == NULL) *lista = nuevoNodo; // Si el nodo a insertar es el primero
+    else aux2->siguiente = nuevoNodo;
     
     nuevoNodo->siguiente = aux1;
 
     printf("\tElemento %d insertado a la lista\n\n", n);
 }
 
-void mostrarLista(Nodo *lista)
-{
+void mostrarLista(Nodo *lista) {
     Nodo *actual = lista; // No es necesario asignar memoria nueva
 
-    if(actual != NULL)
-        printf(".:Elementos de la lista:.\n");
-    else
-        printf("Lista Vacia");
+    if(actual != NULL) printf(".:Elementos de la lista:.\n");
+    else printf("Lista Vacia");
 
-    while (actual != NULL)
-    {
+    while (actual != NULL) {
         printf("%d \t", actual->dato);
         actual = actual->siguiente;
     }
 }
 
-void buscarLista(Nodo *lista, int n)
-{
+void buscarLista(Nodo *lista, int n) {
     Nodo *actual = lista; // No es necesario asignar memoria nueva
     bool flag = false;
 
-    while (actual != NULL && actual->dato <= n)
-    {
-        if (actual->dato == n)
-            flag = true;
+    while (actual != NULL && actual->dato <= n) {
+        if(actual->dato == n) flag = true;
         actual = actual->siguiente;
     }
     
-    if (flag == true)
-        printf("\tSe encontro el elemento %d en la lista!!!\n", n);
-    else
-        printf("\tNo se encontro el elemento %d en la lista!!!\n", n);
+    if(flag == true) printf("\tSe encontro el elemento %d en la lista!!!\n", n);
+    else printf("\tNo se encontro el elemento %d en la lista!!!\n", n);
 }
 
 void eliminarNodo(Nodo **lista, int n) // Cambiar la firma de la función
 {
-    if (*lista != NULL)
-    {
-        Nodo *auxBorrar = *lista, *anterior = NULL;
-        while (auxBorrar != NULL && auxBorrar->dato != n)   
-        {
-            anterior = auxBorrar;
-            auxBorrar = auxBorrar->siguiente;
-        }
-
-        if (auxBorrar == NULL)
-            printf("\tElemento no encontrado\n");
-        else if (anterior == NULL) 
-        {   
-            *lista = (*lista)->siguiente;
-            free(auxBorrar);
-            printf("\tElemento borrado con exito!!!\n");
-        }
-        else
-        {
-            anterior->siguiente = auxBorrar->siguiente;
-            free(auxBorrar);
-            printf("\tElemento borrado con exito!!!\n");
-        }
-    }
-    else
+    if(*lista == NULL) {
         printf("\nLista Vacia\n");
+        return;
+    }
+
+    Nodo *auxBorrar = *lista, *anterior = NULL;
+
+    while (auxBorrar != NULL && auxBorrar->dato != n)    {
+        anterior = auxBorrar;
+        auxBorrar = auxBorrar->siguiente;
+    }
+
+    if(auxBorrar == NULL) printf("\tElemento no encontrado\n");
+    else if(anterior == NULL) {   
+        *lista = (*lista)->siguiente;
+        free(auxBorrar);
+        printf("\tElemento borrado con exito!!!\n");
+    }
+    else {
+        anterior->siguiente = auxBorrar->siguiente;
+        free(auxBorrar);
+        printf("\tElemento borrado con exito!!!\n");
+    }
 }
 
-int menu()
-{
+int menu() {
     int op;
 
     printf("\t.::Menu::.\n");
@@ -107,10 +90,9 @@ int menu()
     printf("4. Eleminar elemento de la lista\n");
     printf("5. Salir\n\n");
     
-    do
-    {
+    do {
         printf("Ingrese una opcion: ");
-        scanf("%d",&op);
+        scanf("%d", &op);
         printf("\n");
     } while(op < 1 || op > 6);
 
@@ -120,12 +102,10 @@ int menu()
 int main() {
     int dato, op;
     
-    do
-    {
+    do {
         op = menu();
 
-        switch(op)
-        {
+        switch(op) {
             case 1:
                 printf("Ingrese un numero: ");
                 scanf("%d", &dato);
