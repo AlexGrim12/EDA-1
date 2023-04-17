d7bac4c95de0f7049d92f11f183afa40ae88272b
Alan Acosta <alanomar460@gmail.com>
2023-04-17 16:38:45 -0600

    T6 v2 - falta revisarlo

--- T6-AcostaA-GutierrezG.c
+++ T6-AcostaA-GutierrezG.c
@@ -1,187 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAXQUEUE 5
-typedef int item;
+#define MAXQUEUE 6
 
-typedef struct
-{
-  item dato;
-  Nodo *link;
-  int emergencia;
+typedef struct Nodo {
+    int numPaciente;
+    struct Nodo *siguiente;
 } Nodo;
 
+// Variables Globales
 Nodo *HEAD = NULL, *TAIL = NULL;
-int queueItems = 0;
+int queueItems = 0, dato, opcion;
 
-void QInsert (item valor)
-{
-  Nodo *pN;
-  if (queueItems >= MAXQUEUE)
-    {
-      printf ("*** Queue saturada; ya no hay espacio ***\n");
-      return;
+void QInsert () {
+    if (queueItems >= MAXQUEUE) {
+        printf ("*** Queue saturada, Limite alcanzado ***\n\n");
+        return;
     }
-  pN = (Nodo *) malloc (sizeof (Nodo));
-  pN->dato = valor;
-  pN->emergencia = 0;
-  pN->link = NULL;
-  if (HEAD == NULL)
-      HEAD = pN;
-  if (TAIL == NULL)
-      TAIL = pN;
-  else
-    {
-      TAIL->link = pN;
-      TAIL = pN;
+
+    char op;
+    Nodo *aux;
+    aux = (Nodo *) malloc (sizeof (Nodo));
+
+    printf("Ingrese la identificacion del paciente\n> ");
+    scanf("%d", &dato);
+    scanf("%*c");
+    
+    queueItems ++;
+
+    printf("\n*** Tiempo estimado de atencion: %d min ***\n", queueItems - 1);
+    printf("Desea ingresar al paciente a la queue? (S/N)\n> ");
+    scanf("%c", &op);
+
+    if (op == 's' || op == 'S') {
+        aux->numPaciente = dato;
+        aux->siguiente = NULL;
+
+        if (HEAD == NULL)
+            HEAD = aux;
+
+        if (TAIL == NULL)
+            TAIL = aux;
+        else {
+            TAIL->siguiente = aux;
+            TAIL = aux;
+        }
+
+        printf("\n*** Paciente %d ingresado con exito ***\n\n", dato);
+    }   
+    else {
+        printf("\n*** No se atendera al paciente ***\n\n");
+        queueItems --;
     }
-  queueItems ++;
-  printf ("pN=%p\tHead=%p\tTail=%p\n\n", pN, HEAD, TAIL);
 }
 
-item QRemove ()
+void QRemove ()
 {
-  item valor;
-  Nodo *pN;
-  if (HEAD == NULL)
-    {
-      printf ("*** Queue Underflow - Cola vacia ***\n");
-      return -999;
-    }
-   
-  if (HEAD->emergencia = 0) 
-    queueItems --;
-  valor = HEAD->dato;
-  pN = HEAD;
-  HEAD = HEAD->link;
-  free (pN);
-  
-  return valor;
-}
-
-void abandonQueue (item valor)
-{
-    Nodo *p1, *p2;
-    int found = 0;
-  
     if (HEAD == NULL)
     {
-      printf ("*** Queue Underflow; Cola vacia ***\n");
-      return;
+        printf ("*** Queue Vacia, Ingrese Pacientes ***\n\n");
+        return;
     }
-    
-    if (HEAD->dato == valor)	// Es el primero
-    {
-        HEAD = p1->link;
-        free (p1);
-        found = 1;
-      queueItems--;
-      return;
-    }
-    
-    p1 = HEAD;
-    p2 = p1->link;
-  
-    while (1)
-    {
-        if (p2->dato == valor)
-	    {
-	        p1->link = p2->link;
-            found = 1;
-	        queueItems--;
-	        
-	        return;
-	    }
-        else
-	    {
-	        p1 = p2;
-	        p2 = p2->link;
-	    }
-	    
-        if (p2 == NULL)
-	        break;
-    }
-}
 
-void emergencia (item valor)
-{
-  Nodo *pN;
-  pN = (Nodo *) malloc (sizeof (Nodo));
-  pN->dato = valor;
-  pN->link = HEAD;
-  pN->emergencia = 1;
-  HEAD = pN;
+    Nodo *aux;
+    
+    dato = HEAD->numPaciente;
+
+    aux = HEAD;
+    HEAD = HEAD->siguiente;
+    free (aux);
+
+    queueItems --;
+
+    printf("*** Paciente %d atendido con exito ***\n\n", dato);
 }
 
 void showQueue ()
 {
-  int k;
-  Nodo *pN;
-  
-  printf ("*** Estado de Queue \tHEAD=%p\tTAIL=%p ***\n", HEAD, TAIL);
-  if (HEAD == NULL)
+    if (HEAD == NULL)
     {
-      printf ("*** Empty Queue ***\n\n");
-      return;
+        printf ("*** Queue Vacia, Ingrese Pacientes ***\n\n");
+        return;
     }
-  pN = HEAD;
-  while (pN != NULL)
+
+    Nodo *aux;
+    
+    aux = HEAD;
+
+    printf("*** Estado de la Queue ***\n");
+    while (aux != NULL)
     {
-      printf ("%d %p\n", pN->dato, pN->link);
-      pN = pN->link;
+        printf ("%d\n", aux->numPaciente);
+        aux = aux->siguiente;
     }
+    printf("\n\n");
+}
+
+int menu () {
+    printf("*********** Queue de Pacientes ********\n");
+    printf("1. Agregar\n2. Atender\n3. Mostrar\n4. Salir\n");
+    do {
+        printf("Opcion?\n> ");
+        scanf("%d", &opcion);
+    } while (opcion < 1 || opcion > 4);
     printf("\n");
+
+    return opcion;
 }
 
 int main ()
 {
-  item valor;
-  int opcion;
-  
-  printf ("*********** Linked Queue ********\n");
-  
-  while (1)
-  {
-    printf ("\t1. QInsert\n");
-    printf ("\t2. QRemove\n");
-    printf ("\t3. Mostrar Queue\n");
-    printf ("\t4. Abandonar Queue\n");
-    printf ("\t5. Emergencia\n");
-    printf ("\t6. Salir\n");
-    printf ("\tOpcion? ");
-    scanf ("%d", &opcion);
-    switch (opcion)
+    do
     {
-      case 1:
-	    printf ("Valor? ");
-	    scanf ("%d", &valor);
-	    QInsert (valor);
-	    break;
-	    
-      case 2:
-	    valor = QRemove ();
-	    if (valor != -999)
-	    printf ("Valor removido del Queue = %d\n\n", valor);
-    	break;
-    	
-      case 3:
-	    showQueue ();
-    	break;
-    	
-      case 4:
-	    printf ("Quien se retira? ");
-    	scanf ("%d", &valor);
-	    abandonQueue (valor);
-	    break;
-	    
-      case 5:
-    	printf ("Quien llega de emergencia? ");
-	    scanf ("%d", &valor);
-	    emergencia (valor);
-	    printf("Valor %d agregado de emergencia\n\n", valor);
-	    break;
-    }
-    
-    if (opcion == 6)
-      break;
-  }
+        opcion = menu();
+
+        switch (opcion)
+        {
+            case 1: QInsert(); break;
+            case 2: QRemove ();break;
+            case 3: showQueue (); break;
+            default: printf("*** Fin del Programa ***"); break;
+        }
+    } while (opcion != 4);
+
+    return 0;
 }
