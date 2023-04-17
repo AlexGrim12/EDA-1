6b9be78e65aad129b519e5395773129c05bcf992
Alan Acosta <alanomar460@gmail.com>
2023-04-17 16:46:44 -0600

    T6 v3 - con comentarios

--- T6-AcostaA-GutierrezG.c
+++ T6-AcostaA-GutierrezG.c
@@ -12,7 +12,7 @@ Nodo *HEAD = NULL, *TAIL = NULL;
 int queueItems = 0, dato, opcion;
 
 void QInsert () {
-    if (queueItems >= MAXQUEUE) {
+    if (queueItems >= MAXQUEUE) { // Comprobar si hay espacio en la queue
         printf ("*** Queue saturada, Limite alcanzado ***\n\n");
         return;
     }
@@ -21,63 +21,54 @@ void QInsert () {
     Nodo *aux;
     aux = (Nodo *) malloc (sizeof (Nodo));
 
+    // Pedir datos al usuario
     printf("Ingrese la identificacion del paciente\n> ");
     scanf("%d", &dato);
     scanf("%*c");
-    
-    queueItems ++;
 
-    printf("\n*** Tiempo estimado de atencion: %d min ***\n", queueItems - 1);
+    // Mostrar tiempo estimado de atencion
+    printf("\n*** Tiempo estimado de atencion: %d min ***\n", queueItems);
     printf("Desea ingresar al paciente a la queue? (S/N)\n> ");
     scanf("%c", &op);
 
-    if (op == 's' || op == 'S') {
+    if (op == 's' || op == 'S') { // Ingresar paciente a la queue
         aux->numPaciente = dato;
         aux->siguiente = NULL;
 
-        if (HEAD == NULL)
-            HEAD = aux;
+        if (HEAD == NULL) HEAD = aux;
 
-        if (TAIL == NULL)
-            TAIL = aux;
+        if (TAIL == NULL) TAIL = aux;
         else {
             TAIL->siguiente = aux;
             TAIL = aux;
         }
 
+        queueItems ++;
         printf("\n*** Paciente %d ingresado con exito ***\n\n", dato);
     }   
-    else {
-        printf("\n*** No se atendera al paciente ***\n\n");
-        queueItems --;
-    }
+    else printf("\n*** No se atendera al paciente ***\n\n");
 }
 
-void QRemove ()
-{
-    if (HEAD == NULL)
-    {
+void QRemove () {
+    if (HEAD == NULL) { // Comprobar si hay pacientes en la queue
         printf ("*** Queue Vacia, Ingrese Pacientes ***\n\n");
         return;
     }
 
     Nodo *aux;
     
+    // Eliminar paciente al inicio de la queue
     dato = HEAD->numPaciente;
-
     aux = HEAD;
     HEAD = HEAD->siguiente;
     free (aux);
 
     queueItems --;
-
     printf("*** Paciente %d atendido con exito ***\n\n", dato);
 }
 
-void showQueue ()
-{
-    if (HEAD == NULL)
-    {
+void showQueue () {
+    if (HEAD == NULL) { // Comprobar si hay pacientes en la queue
         printf ("*** Queue Vacia, Ingrese Pacientes ***\n\n");
         return;
     }
@@ -86,9 +77,9 @@ void showQueue ()
     
     aux = HEAD;
 
+    // Mostrar pacientes en la queue
     printf("*** Estado de la Queue ***\n");
-    while (aux != NULL)
-    {
+    while (aux != NULL) {
         printf ("%d\n", aux->numPaciente);
         aux = aux->siguiente;
     }
@@ -101,23 +92,20 @@ int menu () {
     do {
         printf("Opcion?\n> ");
         scanf("%d", &opcion);
-    } while (opcion < 1 || opcion > 4);
+    } while (opcion < 1 || opcion > 4); // Validar opcion
     printf("\n");
 
     return opcion;
 }
 
-int main ()
-{
-    do
-    {
+int main () {
+    do {
         opcion = menu();
 
-        switch (opcion)
-        {
+        switch (opcion) {
             case 1: QInsert(); break;
-            case 2: QRemove ();break;
-            case 3: showQueue (); break;
+            case 2: QRemove();break;
+            case 3: showQueue(); break;
             default: printf("*** Fin del Programa ***"); break;
         }
     } while (opcion != 4);
